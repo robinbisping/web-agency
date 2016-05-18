3dd8b10af7bdc4413fade13d0450873d072bf229
Robin Bisping <robin@bisping.ch>
2016-05-18 22:20:53 +0200

    Pass URI to Templates, Fixes

--- public/index.php
+++ public/index.php
@@ -1,13 +1,13 @@
 <?php
 
 // Require vendors via composer
-require __DIR__ . '../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 // Register twig template engine
 Twig_Autoloader::register();
 
 // Load and set up twig environment
-$loader = new Twig_Loader_Filesystem(__DIR__ . '../resources/views');
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/../resources/views');
 $twig = new Twig_Environment($loader, array(
 	'debug' => true, // Disable in production
 	'cache' => false // Enable in production: '../storage' instead of false
@@ -15,15 +15,18 @@ $twig = new Twig_Environment($loader, array(
 
 // Route all requests
 $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
-	$r->addRoute('GET', '/', view('index.html', array()));
+	$r->addRoute('GET', '/', view('index.html'));
 }, [
-	'cacheFile' => __DIR__ . '../storage/route.cache', // Adds cache support
-	'cacheDisabled' => IS_DEBUG_ENABLED // Only in production
+	'cacheFile' => __DIR__ . '/../storage/route.cache', // Adds cache support
+	'cacheDisabled' => true // Disable in production
 ]);
 
 // Render view
-function view($file, $variables) {
+function view($file) {
 	global $twig;
+	$variables = array(
+		'uri' => $_SERVER['REQUEST_URI']
+	);
 	$template = $twig->loadTemplate($file);
 	echo $template->render($variables);
 }
