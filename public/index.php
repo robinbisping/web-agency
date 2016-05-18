b22937a76d08aaad337f92d252b96d4658d1d2af
Robin Bisping <robin@bisping.ch>
2016-05-18 22:11:14 +0200

    Add Cache Support for Routes

--- public/index.php
+++ public/index.php
@@ -1,13 +1,13 @@
 <?php
 
 // Require vendors via composer
-require '../vendor/autoload.php';
+require __DIR__ . '../vendor/autoload.php';
 
 // Register twig template engine
 Twig_Autoloader::register();
 
 // Load and set up twig environment
-$loader = new Twig_Loader_Filesystem('../resources/views');
+$loader = new Twig_Loader_Filesystem(__DIR__ . '../resources/views');
 $twig = new Twig_Environment($loader, array(
 	'debug' => true, // Disable in production
 	'cache' => false // Enable in production: '../storage' instead of false
@@ -16,7 +16,10 @@ $twig = new Twig_Environment($loader, array(
 // Route all requests
 $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
 	$r->addRoute('GET', '/', view('index.html', array()));
-});
+}, [
+	'cacheFile' => __DIR__ . '../storage/route.cache', // Adds cache support
+	'cacheDisabled' => IS_DEBUG_ENABLED // Only in production
+]);
 
 // Render view
 function view($file, $variables) {
