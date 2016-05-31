4ca7b1531c28090e0ea953b853360c126b768cf9
Robin Bisping <robin@bisping.ch>
2016-05-31 22:58:22 +0200

    Style index page

--- resources/assets/js/scripts.js
+++ resources/assets/js/scripts.js
@@ -1,7 +1,13 @@
 $(function() {
+
 	// Toggle primary menu
 	$('#toggle-primary-menu').click(function() {
 		$(this).toggleClass('active');
 		$('#primary-menu').toggleClass('toggled');
 	});
+
+	// Adjust intro heights and margins
+	$('.intro.image').height(0.9 * ($(window).height() - $('header').height()));
+	$('.intro.content').css('margin-top', ($('.intro.image').height() - $('.intro.content').height()) * 0.5);
+
 });
