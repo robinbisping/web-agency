1ce53873d520e849cf6dd2500f8aa3f5a82ac708
Robin Bisping <robin@bisping.ch>
2016-06-06 17:25:05 +0200

    Fix routing

--- public/index.php
+++ public/index.php
@@ -21,18 +21,43 @@ $data = array();
 $data['base_url'] = 'http://localhost:3000/';
 $data['current_url'] = $data['base_url'] . trim($_SERVER['REQUEST_URI'], '/');
 
-// Route all requests
+// Add all routes
 $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
-	$r->addRoute('GET', '/', view('index.html'));
+	$r->addRoute('GET', '/', 'index');
+	$r->addRoute('GET', '/webdesign', 'webdesign');
+	$r->addRoute('GET', '/fotografie', 'fotografie');
+	$r->addRoute('GET', '/drucksachen', 'drucksachen');
+	$r->addRoute('GET', '/it', 'it');
+	$r->addRoute('GET', '/offerte', 'offerte');
+	$r->addRoute('GET', '/team', 'team');
 }, [
 	'cacheFile' => __DIR__ . '/../storage/route.cache', // Adds cache support
 	'cacheDisabled' => true // Disable in production
 ]);
 
-// Render view
-function view($file) {
-	global $twig;
-	global $data;
-	$template = $twig->loadTemplate($file);
-	echo $template->render($data);
+// Fetch method and URI
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+	$uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+	case FastRoute\Dispatcher::NOT_FOUND:
+		echo '404';
+		break;
+	case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+		$allowedMethods = $routeInfo[1];
+		echo '405';
+		break;
+	case FastRoute\Dispatcher::FOUND:
+		$file = $routeInfo[1] . '.html';
+		$vars = $routeInfo[2];
+		$template = $twig->loadTemplate($file);
+		echo $template->render($data);
+		break;
 }
